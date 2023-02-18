ca996cc30314c369f94aaad7dc2dce0d397f93d6
mbarnesdev <martinbarnes.dev@gmail.com>
2023-02-18 15:14:18 +0000

    swear i can smell hash anaw

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ChakraProvider } from "@chakra-ui/react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
